6d57e4d80d42123ac400a2b3bbcbb486bbad0680
Ville Hämäläinen <ville.t.hamalainen@gmail.com>
2013-03-19 08:31:46 +0200

    Trying out some drawing with js

---
+++ js/health-tree-visualizations.js
@@ -0,0 +1,7 @@
+//This js-file uses Raphaël.js so remember to include it to your webpage
+
+//Draw main health tree visualization
+function drawMainTree(){
+   var mainTree = Raphael("main-tree", 500,500);
+   var rect1 = mainTree.rect(20,30,100,12).attr({fill: "orange"});
+}
