1087f4e37c142e64df03c403ef4a6457e0160d51
Ville Hämäläinen <ville.t.hamalainen@gmail.com>
2013-03-24 16:50:17 +0200

    Getting idea of js functions

--- js/health-tree-visualizations.js
+++ js/health-tree-visualizations.js
@@ -1,7 +1,22 @@
 //This js-file uses Raphaël.js so remember to include it to your webpage
 
 //Draw main health tree visualization
-function drawMainTree(){
+function drawMainTree(municipalityId){
    var mainTree = Raphael("main-tree", 600,500);
+   drawRoots(mainTree, municipalityId);
+   drawTrunk(mainTree, municipalityId);
+   drawBranches(mainTree, municipalityId);
+
+}
+
+function drawRoots(mainTree, municipalityId){
    var rect1 = mainTree.rect(20,30,100,12).attr({fill: "orange"});
+}
+
+function drawTrunk(mainTree, municipalityId){
+   var rect1 = mainTree.rect(30,40,100,12).attr({fill: "orange"});
+}
+
+function drawBranches(mainTree, municipalityId){
+   var rect1 = mainTree.rect(40,50,100,12).attr({fill: "orange"});
 }
