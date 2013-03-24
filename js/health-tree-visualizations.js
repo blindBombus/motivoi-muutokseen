3592955a4b8fa55c70299bbbc0bdae13a0a558fa
Ville Hämäläinen <ville.t.hamalainen@gmail.com>
2013-03-24 19:27:49 +0200

    new logo-suggestion and more js

--- js/health-tree-visualizations.js
+++ js/health-tree-visualizations.js
@@ -3,20 +3,25 @@
 //Draw main health tree visualization
 function drawMainTree(municipalityId){
    var mainTree = Raphael("main-tree", 600,500);
-   drawRoots(mainTree, municipalityId);
-   drawTrunk(mainTree, municipalityId);
-   drawBranches(mainTree, municipalityId);
+   drawBackground(mainTree, municipalityId);
+   var trunkStartpoints  = drawRoots(mainTree, municipalityId);
+   var branchStartpoints = drawTrunk(mainTree, municipalityId, trunkStartpoints);
+   drawBranches(mainTree, municipalityId, branchStartpoints);
 
 }
+function drawBackground(mainTree, municipalityId){
 
+}
 function drawRoots(mainTree, municipalityId){
    var rect1 = mainTree.rect(20,30,100,12).attr({fill: "orange"});
+   return 1;
 }
 
-function drawTrunk(mainTree, municipalityId){
+function drawTrunk(mainTree, municipalityId, trunkStartpoints){
    var rect1 = mainTree.rect(30,40,100,12).attr({fill: "orange"});
+   return 1;
 }
 
-function drawBranches(mainTree, municipalityId){
+function drawBranches(mainTree, municipalityId, branchStartpoints){
    var rect1 = mainTree.rect(40,50,100,12).attr({fill: "orange"});
 }
