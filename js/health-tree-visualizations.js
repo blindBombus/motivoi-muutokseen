e063a27e0a6376e2d992cfa3e5e0a6d47fe078ff
Ville Hämäläinen <ville.t.hamalainen@gmail.com>
2013-03-26 15:12:38 +0200

    drawing rough tree

--- js/health-tree-visualizations.js
+++ js/health-tree-visualizations.js
@@ -4,9 +4,9 @@
 function drawMainTree(municipalityId){
    var mainPaper = Raphael("main-tree", 600,500);
    drawBackground(mainPaper, municipalityId);
-   var trunkStartpoints  = drawRoots(mainPaper, municipalityId);
-   var branchStartpoints = drawTrunk(mainPaper, municipalityId, trunkStartpoints);
-   drawBranches(mainPaper, municipalityId, branchStartpoints);
+   var trunkWidth  =  drawTrunk(mainPaper, municipalityId);
+   drawRoots(mainPaper, municipalityId, trunkWidth);
+   drawBranches(mainPaper, municipalityId, trunkWidth);
 
 }
 
@@ -14,29 +14,35 @@ function drawMainTree(municipalityId){
 //to mainPaper. Uses municipalityId to get the indicator data from
 //correct municipality.
 function drawBackground(mainPaper, municipalityId){
+   var ground = mainPaper.ellipse(300, 440, 150, 50).attr({fill: '#46a046', stroke: '#83a67f', 'stroke-width': 8});  
+}
+
+//Draws trunk visualization to mainPaper. Uses municipalityId 
+//to get the indicator data from correct municipality.
+//Returns the distance between trunkparts.
+function drawTrunk(mainPaper, municipalityId){
+   var trunkWidth = 15;
+   var trunkCenter = mainPaper.path("M300 400 l 0 -125").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var trunkLeft = mainPaper.path("M"+(300-trunkWidth)+" 400 l 0 -100").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var trunkRight = mainPaper.path("M"+(300+trunkWidth)+" 400 l 0 -150").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   return trunkWidth;
 }
 
 //Draws root visualizations and other indicator visualizations that
 //must be drawn with the roots to mainPaper. Uses municipalityId 
 //to get the indicator data from correct municipality.
-//Returns the coordinates of the points where the trunk-parts should
-//start.
-function drawRoots(mainPaper, municipalityId){
-   var rect1 = mainPaper.rect(20,30,100,12).attr({fill: "orange"});
-   return 1;
+function drawRoots(mainPaper, municipalityId, trunkWidth){
+   var rootCenter = mainPaper.path("M300 400 l 25 75").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var rootLeft = mainPaper.path("M"+(300-trunkWidth)+" 400 l -75 25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var rootRight = mainPaper.path("M"+(300+trunkWidth)+" 400 l 75 50").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
 }
 
-//Draws trunk visualization to mainPaper. Uses municipalityId 
-//to get the indicator data from correct municipality.
-//Returns the coordinates of the points where the branch-parts should
-//start.
-function drawTrunk(mainPaper, municipalityId, trunkStartpoints){
-   var rect1 = mainPaper.rect(30,40,100,12).attr({fill: "orange"});
-   return 1;
-}
+
 
 //Draws branch visualizations and leaves for every branch to mainPaper. 
 //Uses municipalityId to get the indicator data from correct municipality.
-function drawBranches(mainPaper, municipalityId, branchStartpoints){
-   var rect1 = mainPaper.rect(40,50,100,12).attr({fill: "orange"});
+function drawBranches(mainPaper, municipalityId, trunkWidth){
+   var youngBranch = mainPaper.path("M300 275 l 0 -200").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var generalBranch = mainPaper.path("M"+(300-trunkWidth)+" 300 l -25 -25 l -100 -25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var elderlyBranch = mainPaper.path("M"+(300+trunkWidth)+" 250 l 25 -25 l 100 -25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
 }
