416c69237b7b648f8c411f267370d73fc26f0739
Ville Hämäläinen <ville.t.hamalainen@gmail.com>
2013-04-01 12:42:38 +0300

    fixed drawing to one paper

--- js/health-tree-visualizations.js
+++ js/health-tree-visualizations.js
@@ -1,13 +1,12 @@
 //This js-file uses Raphaël.js so remember to include it to your webpage
 
 //Draws main health tree visualization
-function drawMainTree(municipalityId){
-   var mainPaper = Raphael("main-tree", 600,500);
+function drawMainTree(municipalityId, mainPaper){
+   mainPaper.clear();
    drawBackground(mainPaper, municipalityId);
    var trunkWidth  =  drawTrunk(mainPaper, municipalityId);
    drawRoots(mainPaper, municipalityId, trunkWidth);
    drawBranches(mainPaper, municipalityId, trunkWidth);
-
 }
 
 //Draws background elements and miscellaneous indicator visualizations
@@ -22,9 +21,9 @@ function drawBackground(mainPaper, municipalityId){
 //Returns the distance between trunkparts.
 function drawTrunk(mainPaper, municipalityId){
    var trunkWidth = 15;
-   var trunkCenter = mainPaper.path("M300 400 l 0 -125").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
-   var trunkLeft = mainPaper.path("M"+(300-trunkWidth)+" 400 l 0 -100").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
-   var trunkRight = mainPaper.path("M"+(300+trunkWidth)+" 400 l 0 -150").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var trunkCenter = mainPaper.path("M300 380 l 0 -75").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var trunkLeft = mainPaper.path("M"+(300-trunkWidth)+" 375 l 0 -50").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var trunkRight = mainPaper.path("M"+(300+trunkWidth)+" 375 l 0 -100").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
    return trunkWidth;
 }
 
@@ -32,17 +31,17 @@ function drawTrunk(mainPaper, municipalityId){
 //must be drawn with the roots to mainPaper. Uses municipalityId 
 //to get the indicator data from correct municipality.
 function drawRoots(mainPaper, municipalityId, trunkWidth){
-   var rootCenter = mainPaper.path("M300 400 l 25 75").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
-   var rootLeft = mainPaper.path("M"+(300-trunkWidth)+" 400 l -75 25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
-   var rootRight = mainPaper.path("M"+(300+trunkWidth)+" 400 l 75 50").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var rootCenter = mainPaper.path("M300,380 c0,0 0,15 5,30 l30,65").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var rootLeft = mainPaper.path("M"+(300-trunkWidth)+" 375 c0,0 0,25 -15,30 l-60,20").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var rootRight = mainPaper.path("M"+(300+trunkWidth)+" 375 c0,0 0,25 15,35 l55, 30").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
 }
-
-
 
 //Draws branch visualizations and leaves for every branch to mainPaper. 
 //Uses municipalityId to get the indicator data from correct municipality.
 function drawBranches(mainPaper, municipalityId, trunkWidth){
-   var youngBranch = mainPaper.path("M300 275 l 0 -200").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
-   var generalBranch = mainPaper.path("M"+(300-trunkWidth)+" 300 l -25 -25 l -100 -25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
-   var elderlyBranch = mainPaper.path("M"+(300+trunkWidth)+" 250 l 25 -25 l 100 -25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var youngBranch = mainPaper.path("M300 305 l 0 -175").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var generalBranch = mainPaper.path("M"+(300-trunkWidth)+" 325 c0,0 0,-20 -25 -25 l -100 -25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
+   var elderlyBranch = mainPaper.path("M"+(300+trunkWidth)+" 275 c0,0 0,-20 25 -25 l 100 -25").attr({stroke: '#524132', 'stroke-width': 8, 'stroke-linecap': 'round'});
 }
+
+
