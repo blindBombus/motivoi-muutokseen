9d4de103c1e5006d0f5d51d9054ff2b90da649e6
Ville Hämäläinen <ville.t.hamalainen@gmail.com>
2013-05-19 17:16:10 +0300

    placed infoboxes to right places

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                <h2>Kihniö</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="7"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="vir"><a id="small_23" href="#2"></a>
@@ -40,7 +40,7 @@
                <h2>Virrat</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="23"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="mv"><a id="small_10" href="#3"></a>
@@ -48,7 +48,7 @@
                <h2>Mänttä-Vilppula</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="10"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="jam"><a id="small_5" href="#4"></a>
@@ -56,7 +56,7 @@
                <h2>Jämsä</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="5"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="kuh"><a id="small_8" href="#5"></a>
@@ -64,7 +64,7 @@
                <h2>Kuhmoinen</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="8"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="par"><a id="small_13" href="#6"></a>
@@ -72,7 +72,7 @@
                <h2>Parkano</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="13"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="ylo"><a id="small_24" href="#7"></a>
@@ -80,7 +80,7 @@
                <h2>Ylöjärvi</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="24"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="ruo"><a id="small_17" href="#8"></a>
@@ -88,7 +88,7 @@
                <h2>Ruovesi</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="17"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="juu"><a id="small_4" href="#9"></a>
@@ -96,7 +96,7 @@
                <h2>Juupajoki</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="4"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="ori"><a id="small_12" href="#10"></a>
@@ -104,7 +104,7 @@
                <h2>Orivesi</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="12"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="kan"><a id="small_6" href="#11"></a>
@@ -112,7 +112,7 @@
                <h2>Kangasala</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="6"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="pal"><a id="small_16" href="#12"></a>
@@ -120,7 +120,7 @@
                <h2>Pälkäne</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="16"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="ika"><a id="small_3" href="#13"></a>
@@ -128,7 +128,7 @@
                <h2>Ikaalinen</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="3"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="ham"><a id="small_2" href="#14"></a>
@@ -136,7 +136,7 @@
                <h2>Hämeenkyrö</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="2"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="nok"><a id="small_11" href="#15"></a>
@@ -144,7 +144,7 @@
                <h2>Nokia</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="11"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="tam"><a id="small_19" href="#16"></a>
@@ -152,7 +152,7 @@
                <h2>Tampere</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="19"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="pir"><a id="small_14" href="#17"></a>
@@ -160,7 +160,7 @@
                <h2>Pirkkala</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="14"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="lem"><a id="small_9" href="#18"></a>
@@ -168,7 +168,7 @@
                <h2>Lempäälä</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="9"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="val"><a id="small_21" href="#19"></a></a>
@@ -176,7 +176,7 @@
                <h2>Valkeakoski</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="21"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="sas"><a id="small_18" href="#20"></a></a>
@@ -184,7 +184,7 @@
                <h2>Sastamala</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="18"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="ves"><a id="small_22" href="#21"></a>
@@ -192,7 +192,7 @@
                <h2>Vesilahti</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="22"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="aka"><a id="small_1" href="#22"></a>
@@ -200,7 +200,7 @@
                <h2>Akaa</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="1"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="pun"><a id="small_15" href="#23"></a>
@@ -208,7 +208,7 @@
                <h2>Punkalaidun</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="15"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
          <li id="urj"><a id="small_20" href="#24"></a>
@@ -216,16 +216,13 @@
                <h2>Urjala</h2>
                <form name="municipality" method="POST" action="detailview.php">
                   <input type="hidden" name="municipality" value="20"></input>
-                  <button type="submit">Yksityiskohtaisempaan tarkasteluun</button>
+                  <button type="submit">Yksityiskohtaisempaan tarkasteluun &gt;&gt;</button>
                </form>
             </div></li>
       </ul> 
 
    </div>
    
-   <div id="dialog" title="Basic dialog">
-      <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
-   </div>
    <script>
       $(document).ready(function () {
          $("#pshp-map li").click(function() {
